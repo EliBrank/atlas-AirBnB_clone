2113b370efcb5c617b89656492076a07e356a798
{DTBissy} <{isaactheedwards@gmail.com}>
2024-05-30 17:42:01 -0500

    Deleted self from self.base_claa

--- models/base_model.py
+++ models/base_model.py
@@ -25,7 +25,7 @@ class BaseModel:
         return {**class_dict, **attr_dict}
 
     def __str__(self):
-        return f"[{self.__class__.__name__}] ({self.id})({self.__dict__})"
+        return f"[{__class__.__name__}] ({self.id})({self.__dict__})"
 
 # %Y-%m-%dT%H:%M:%S.%f
 # id = BaseModel()
