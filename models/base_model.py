7f99672400a17a297c31ff962a2fecf4547ca5b7
{DTBissy} <{isaactheedwards@gmail.com}>
2024-05-30 14:48:18 -0500

    sAY SUM

--- models/base_model.py
+++ models/base_model.py
@@ -6,3 +6,16 @@ import datetime, uuid
 
 
 class BaseModel:
+    __id = 0
+    def __init__(self, id=uuid.uuid4()):
+        if id is None:
+            BaseModel.__id += 1
+            self.id = BaseModel.__id
+        else:
+            self.id = id
+
+    # def __str__(self):
+    #     return f"[{cls.__name__}] ({self.id})({self.__dict__})"
+
+id = BaseModel()
+print(id.id)
