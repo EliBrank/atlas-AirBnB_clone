3ca7211b5f5b6e654c0a523d2505bcd60938b532
{DTBissy} <{isaactheedwards@gmail.com}>
2024-05-31 14:05:51 -0500

    I made the basic console template

---
+++ console.py
@@ -0,0 +1,19 @@
+import sys, cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """Command or Console"""
+    prompt = '(hbnb) ' if sys.__stdin__.isatty() else ''
+
+    def do_quit(self, command):
+        """Quitter"""
+        exit()
+
+    def do_EOF(self, arg):
+        """Handles EOF to exit prog"""
+        print()
+        exit()
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
