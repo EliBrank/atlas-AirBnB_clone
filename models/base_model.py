3162a1fb99d7cc56e87f60ee5dc65318a559b975
EliBrank <eliasbrinkman@gmail.com>
2024-06-01 15:26:39 -0500

    re-write to_dict method to convert datetimes within method instead of at assignment

--- models/base_model.py
+++ models/base_model.py
@@ -11,13 +11,13 @@ class BaseModel:
     def __init__(self):
         """initializes BaseModel with unique id and creation date"""
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.now().isoformat()
+        self.created_at = datetime.now()
         # self.created_at = datetime.strptime(f"{self.created_at}", "%Y-%m-%dT%H:%M:%S.%f")
         self.updated_at = self.created_at
 
     def save(self):
         """sets updated_at attribute to current time"""
-        self.updated_at = datetime.now().isoformat()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """creates dictionary representation of BaseModel
@@ -25,8 +25,19 @@ class BaseModel:
         Returns:
             all BaseModel attributes and class name as dictionary
         """
-        attr_dict = {attr: getattr(self, attr) for attr in self.__dict__}
+        attr_dict = {}
+
+        # first adds class name to dictionary
         attr_dict.update({'__class__' : self.__class__.__name__})
+
+        # adds all other attributes
+        # created_at/updated_at values converted to isoformat here
+        for attr, attr_value in self.__dict__.items():
+            if attr == "created_at" or attr == "updated_at":
+                attr_dict.update({attr : attr_value.isoformat()})
+            else:
+                attr_dict.update({attr : attr_value})
+        # attr_dict = {attr: getattr(self, attr) for attr in self.__dict__}
         return attr_dict
 
     def __str__(self):
