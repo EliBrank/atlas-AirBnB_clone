88c9d1c49394ab68b885be62bfce0a494ffacca6
EliBrank <eliasbrinkman@gmail.com>
2024-06-02 16:19:23 -0500

    test, workshop file storage methods, add imports to __init__.py in models dir

---
+++ models/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""initializes FileStorage class"""
+
+from models.engine.file_storage import FileStorage
+
+
+storage = FileStorage()
+
+storage.reload()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+
 """This module defines a class named FileStorage"""
-import json
-import os
+
+import json, models
+from os.path import exists
 from models.base_model import BaseModel
 
 
@@ -11,5 +13,34 @@ class FileStorage():
     __objects = {}
 
     def all(self):
-        """This method returns the dictionary __objects"""
+        """returns the dictionary __objects"""
         return self.__objects
+
+    def new(self, obj):
+        """saves obj to __objects with object's class and unique id as key
+
+        Args:
+            obj: python object to add to dictionary
+        """
+
+        self.__objects.update({f"{obj.__class__.__name__}.{obj.id}" : obj})
+
+    def save(self):
+        """saves each object in __objects to JSON file
+
+        Args:
+            __objects: python objects to be converted and saved
+        """
+
+        json_list = []
+        with open(self.__file_path, "w") as f:
+            for obj in self.__objects:
+                json_list.append(obj.to_dict())
+            json.dump(json_list, f)
+
+    def reload(self):
+        """reloads __objects dictionary with objects from JSON file"""
+
+        if exists(self.__file_path):
+            with open(self.__file_path, "r") as f:
+                self.__objects = json.load(f)
