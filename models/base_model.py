25af7d342d879248617e5b9fbfc922c5d306dfdd
{DTBissy} <{isaactheedwards@gmail.com}>
2024-06-02 00:08:14 -0500

    My private situation

--- models/base_model.py
+++ models/base_model.py
@@ -5,38 +5,31 @@
 from datetime import datetime, date, time
 import uuid
 
-
 class BaseModel:
     """base class for city, place, etc."""
     def __init__(self, *args, **kwargs):
         """initializes BaseModel with unique id and creation date"""
+        from models import storage
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
-        if kwargs is not None:
-
-            # If created_at or updated_at are in kwargs,
-            # they will be overwritten with datetime format
-            kwargs["created_at"] = datetime.strptime(
-                kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f"
-            )
-            kwargs["updated_at"] = datetime.strptime(
-                kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f"
-            )
-
-            # loop through user input for any other key/value pairs
-            # initialize anything entered except for __class__
+        if kwargs:
             for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 if key != "__class__":
                     setattr(self, key, value)
+            if "id" not in kwargs:
+                self.id = str(uuid.uuid4())
+                self.created_at = datetime.now()
+                self.updated_at = datetime.now()
 
         else:
             # normal instantiation if no kwargs
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
-
-    def save(self):
-        """sets updated_at attribute to current time"""
-        self.updated_at = datetime.now()
 
     def to_dict(self):
         """creates dictionary representation of BaseModel
@@ -66,3 +59,7 @@ class BaseModel:
             object formatted as [<class name>] (<self.id>) <self.__dict__>
         """
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """Saves sum"""
+        self.updated_at = datetime.now()
