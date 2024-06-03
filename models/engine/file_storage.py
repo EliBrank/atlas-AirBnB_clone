038fe63dc16d516c6ba48b3c7443ab1af4c53d6a
EliBrank <eliasbrinkman@gmail.com>
2024-06-03 10:34:22 -0500

    re-work reload method in file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -28,16 +28,24 @@ class FileStorage():
     def save(self):
         """saves each object in __objects to JSON file"""
 
-        json_dict = {}
+        obj_dict = {}
         with open(self.__file_path, "w") as f:
             for key, value in self.__objects.items():
-                json_dict.update({f"{key}" : value.to_dict()})
+                obj_dict.update({f"{key}" : value.to_dict()})
 
-            json.dump(json_dict, f)
+            f.write(json.dumps(obj_dict))
 
     def reload(self):
         """reloads __objects dictionary with objects from JSON file"""
 
+        class_dict = {"BaseModel" : models.BaseModel,}
+
         if exists(self.__file_path):
             with open(self.__file_path, "r") as f:
-                self.__objects = json.load(f)
+                obj_dict = json.load(f)
+                for key, value in obj_dict.items():
+                    # separate class name from key
+                    class_name = key.split(".")[0]
+                    if class_name in class_dict:
+                        class_name = class_dict[class_name]
+                        self.__objects[key] = class_name(**value)
