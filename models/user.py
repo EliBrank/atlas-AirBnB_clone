95ee210e3bbcdcd0d68ad9d206975e65b3fe4aaa
EliBrank <eliasbrinkman@gmail.com>
2024-06-03 12:53:54 -0500

    reformat class to use public class attributes instead of instance attributes

--- models/user.py
+++ models/user.py
@@ -7,22 +7,7 @@ import models
 
 class User(models.BaseModel):
     """defines User based on BaseModel"""
-    def __init__(self, email="", password="", first_name="",
-                last_name="", *args, **kwargs):
-        """initializes User
-
-        Args:
-            email: email address for User (string)
-            password: password for User (string)
-            first_name: User's first name (string)
-            last_name: User's last name (string)
-        """
-
-        super().__init__()
-
-        self.email = email
-        self.password = password
-        self.first_name = first_name
-
-        if last_name != "":
-            self.last_name = last_name
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
