54b40770ff5db88727d3a443ddf4a8b3633937c5
EliBrank <eliasbrinkman@gmail.com>
2024-06-04 14:25:48 -0500

    check that other attributes of User are correct attributes

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -9,13 +9,13 @@ from models import User
 class TestUser(unittest.TestCase):
     """defines unittests for methods in User"""
 
-    def test_email(self):
-        """test assigning email"""
+    def test_attr(self):
+        """test that all attributes of User are correct attribute"""
         user_1 = User()
-        # self.assertTrue("email" in u.__dir__())
-        # u.email = "airbnb@mail.com"
-        # self.assertEqual(u.email, "airbnb@mail.com")
         self.assertIsInstance(user_1.email, str)
+        self.assertIsInstance(user_1.password, str)
+        self.assertIsInstance(user_1.first_name, str)
+        self.assertIsInstance(user_1.last_name, str)
 
 # Test User: User.email
 # Test User: User.password
